846084c0f357b8739f5181e3242949af2fce16e7
Mehul Poddar <mehulpoddar1310@gmail.com>
2022-04-03 10:00:43 +0530

    body search box

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Body from "./components/Body";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="min-h-screen background">
       <Navbar />
+      <Body />
     </div>
   );
 }
