4aa46d11919437e4e52d7fa179afdf8cebfb5ca2
FuryACE <sudhanshuvshekhar@gmail.com>
2022-05-18 00:15:15 +0530

    minor changes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,13 +7,18 @@ const Body = () => {
   return (
     <div className=" ml-[176px] pt-[39px]">
       <div className="flex flex-row ">
-       <div>
-         <SearchBar />
-       </div>
         <div>
-         <Actions />
-         </div>
-              </div>
+          <SearchBar />
+        </div>
+        <div>
+          <Actions />
+        </div>
+      </div>
+      <div className="flex flex-row ">
+        <div className=" w-[909px] h-[313px] mt-[54px] bg-white rounded-3xl" ></div>
+        {/* <div classname=" bg-white"></div> */}
+      </div>
+        
     </div>
   );
 };
